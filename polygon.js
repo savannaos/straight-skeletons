b0a628f380c136354bfd74a22683c515431c7817
savannaos <savanna.smith@uconn.edu>
2017-11-22 16:57:55 -0500

    added polygon instantiation

--- polygon.js
+++ polygon.js
@@ -1,7 +1,14 @@
 class polygon{
-	var lines = new Array();
-	constructor(line[] array){
-		this.lines = array;
+	var edges;
+	constructor(e){
+		this.edges = e;
+		beginShape();
+		for(var i = 0; i<this.edges.length;i++){
+			vertex(this.edges[i].x1,this.edges[i].y1);
+			if(i<this.edges.length-1){								//don't double count first point
+				vertex(this.edges[i].x2, this.edges[i].y2);
+			}
+		endShape(CLOSE);
 	}
-		
+
 }
