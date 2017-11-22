e25743955a3e2fe7ddc29547d426b7996760c0da
savannaos <savanna.smith@uconn.edu>
2017-11-22 15:47:40 -0500

    lines connecting clicked points

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 function setup() {
   createCanvas(600,400);
   background(171,212,242);
+    x = 0;
+    y = 0;
+    prevx = 0;
+    prevy = 0;
 }
 
 function draw(){
@@ -8,6 +12,13 @@ function draw(){
 }
 
 function mousePressed(){
-  ellipse(mouseX, mouseY, 5, 5);
+    x = mouseX;               //get clicked spot
+	  y = mouseY;
+    ellipse(x, y, 5, 5);      //draw point at that spot
+    if(prevy != 0) {          //draw line connecting previous clicked spot and current
+        line(x,y,prevx,prevy);
+    }
+    prevx = x;
+    prevy = y;
   return false;
 }
