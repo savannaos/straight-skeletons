ba55e41d959cd8a688e791ab63b31b6ab232d1ab
savannaos <savanna.smith@uconn.edu>
2017-11-22 16:23:11 -0500

    comments

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 function setup() {
   createCanvas(600,400);
   background(171,212,242);
-    x = 0;
-    y = 0;
-    prevx = 0;
-    prevy = 0;
+  x = 0;
+  y = 0;
+  prevx = 0;
+  prevy = 0;
+
 }
 
 function draw(){
@@ -12,13 +13,13 @@ function draw(){
 }
 
 function mousePressed(){
-    x = mouseX;               //get clicked spot
-	  y = mouseY;
-    ellipse(x, y, 5, 5);      //draw point at that spot
-    if(prevy != 0) {          //draw line connecting previous clicked spot and current
-        line(x,y,prevx,prevy);
-    }
-    prevx = x;
-    prevy = y;
+  x = mouseX;               //get clicked spot
+	y = mouseY;
+  ellipse(x, y, 5, 5);      //draw point at that spot
+  if(prevy != 0) {          //unless it's the first click,
+    line(x,y,prevx,prevy);  //draw line connecting previous clicked spot and current
+  }
+  prevx = x;
+  prevy = y;
   return false;
 }
