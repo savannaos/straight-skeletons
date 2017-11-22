857785df13e2e6a67799d3e6350cfe520d50216f
Elaine Tsun <elaine.tsun3@gmail.com>
2017-11-22 16:12:16 -0500

    Added line and polygon classes

---
+++ line.js
@@ -0,0 +1,15 @@
+class line{
+	var x1;
+	var x2;
+	var x3;
+	var x4;
+	
+	constructor(x1,y1,x2,y2){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+	
+	
+}
---
+++ polygon.js
@@ -0,0 +1,7 @@
+class polygon{
+	var lines = new Array();
+	constructor(line[] array){
+		this.lines = array;
+	}
+		
+}
