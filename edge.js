18f4fb4fc19b97c6e2ea6f5b6cbddc4c55460c1c
Elaine Tsun <elaine.tsun3@gmail.com>
2017-11-22 17:03:49 -0500

    added edge.js

---
+++ edge.js
@@ -0,0 +1,17 @@
+class edge{
+	
+	constructor(x1,y1,x2,y2){
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+	function will_close(beginning_line){
+		var e = this;
+		var begin = beginning_edge;
+		// add stuff here comparing the two
+		return false;
+	}
+	
+
+}
--- sketch.js
+++ sketch.js
@@ -11,14 +11,24 @@ function draw(){
 
 }
 
+var line_array = new Array();
+var done = false;
+
 function mousePressed(){
     x = mouseX;               //get clicked spot
-	  y = mouseY;
-    ellipse(x, y, 5, 5);      //draw point at that spot
-    if(prevy != 0) {          //draw line connecting previous clicked spot and current
-        line(x,y,prevx,prevy);
-    }
-    prevx = x;
-    prevy = y;
+	y = mouseY;
+	if(done == false){
+	    ellipse(x, y, 5, 5);      //draw point at that spot
+		    if(prevy != 0) {          //draw line connecting previous clicked spot and current
+		        line(x,y,prevx,prevy);
+		        var e = new edge(x,y,prevx,prevy);
+		        line_array.push(e);	  //add line to the array
+		        if(e.will_close(line_array[0])){
+		        	done = true;
+		        }
+		    }
+	    prevx = x;
+	    prevy = y;
+	}
   return false;
 }
